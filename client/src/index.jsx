51e59515c48087cbdd77a0ac1d5737583904cb21
fmarcos <fmarcosdev@gmail.com>
2021-04-20 11:18:14 -0400

    montaje de redux listo

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import storeFn from './redux/store/index';
+import { Provider } from 'react-redux';
+
+const store = storeFn();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
